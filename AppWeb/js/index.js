19cf7d65cff99d11adec82409aabec68e019aafd
Carlos App <107434698+CarlosEApp@users.noreply.github.com>
2025-07-01 15:10:11 -0300

    Add files via upload

--- AppWeb/js/index.js
+++ AppWeb/js/index.js
@@ -236,6 +236,56 @@ didOpen: () => {
     document.body.style.paddingRight = '0px';
  }
 });  
+document.getElementById('PsqCódigo').addEventListener('click',function(){
+   
+     sessionStorage.setItem('pesQuiSar', '');
+    Swal.fire({
+title: `Pesquise por Código`,
+html:` <div  class="menu-container">
+  <p>Digite Código</p>
+  <br>
+  <input id='idcódigo' type='text' placeholder='Código...'>
+  <br><br>
+  <button id="PasqCodigo" title="">Start <i id="pesq-1" onclick="pesquisar()" class="fa-solid fa-magnifying-glass"></i> </button>
+  <br><br>  
+  <button id='Sair' class='cancelar'> Cancelar </button>
+</div>
+`,
+showCancelButton: false,
+showConfirmButton: false,
+customClass: {
+popup: 'my-custom_PesqCódigo' // Aplica a classe CSS personalizada
+},
+didOpen: () => {
+  document.body.style.paddingRight = '0px';
+}
+});  
+ document.getElementById('idcódigo').value='';
+document.getElementById('Sair').addEventListener('click',function(){
+Swal.close()
+
+});
+document.getElementById('PasqCodigo').addEventListener('click',function(){
+sessionStorage.setItem('itens',``)
+var respDoc= document.getElementById('idcódigo').value;
+if(!respDoc|| respDoc==''){
+ sessionStorage.setItem('pesQuiSar', '');
+ Swal.fire('Campo código vazio!')
+}else{
+  sessionStorage.setItem('pesQuiSar', respDoc);
+  setTimeout(function(){
+   sessionStorage.setItem('pesQuiSar', respDoc);
+   buscar()
+  },1000)
+
+
+}
+
+
+})
+})
+
+
 document.getElementById('Sair').addEventListener('click',function(){
 Swal.close('click')
 });
@@ -403,4 +453,118 @@ setInterval(function() {
 }
 } 
 
+
+
+
+
+// lista inicial firebase
+
+var list= document.getElementById('listaInicial');
+list.innerHTML = '';
+
+sessionStorage.setItem('itens',``)
+var firebaseConfig = {
+apiKey: "AIzaSyD7q-qzsIhACByHciJkDBI3yPuKK_bgHUM",
+authDomain: "logos-asd.firebaseapp.com",
+projectId: "logos-asd",
+storageBucket: "logos-asd.firebasestorage.app",
+messagingSenderId: "801633317687",
+appId: "1:801633317687:web:3a38f39b2e9861902e20b2",
+measurementId: "G-GJEW931Y73"
+};
+firebase.initializeApp(firebaseConfig);  
+
+var produtosRef = db.collection(`Codigos`);
+produtosRef.get().then((querySnapshot) => {
+querySnapshot.forEach(doc => {
+var doc = doc.data();
+var itens= querySnapshot.size;
+
+
+var div1=document.createElement('div');
+var div2=document.createElement('div');
+var div3=document.createElement('div');        
+var img=document.createElement('img');
+var imgbotão=document.createElement('img');
+var botão1= document.createElement('button');
+var botão2= document.createElement('button'); 
+var botão3= document.createElement('button'); 
+var label1= document.createElement('label');
+var label2= document.createElement('label'); 
+var label3= document.createElement('label');
+var br1=  document.createElement('br');
+var br2=  document.createElement('br');  
+var br3=  document.createElement('br');  
+var br4=  document.createElement('br');
+var br5=  document.createElement('br'); 
+
+div1.id='div1List_';
+div3.id='div3List_';
+div2.id='div2List';
+img.id='imgList';
+label1.id='label1List';
+label2.id='label2List'; 
+label3.id='label3List';
+botão1.id='botão1List';
+botão2.id='botão2List'; 
+botão3.id='botão3List'; 
+imgbotão.id='imgList2';
+
+img.src=`${doc.URL}`;
+
+label1.textContent=`${doc.Titulo}`;
+label2.textContent=`${doc.Nome}`;
+label3.textContent=`Código: ${doc.Código}`;
+if(!doc.Canvas||doc.Canvas==''){
+ 
+  imgbotão.src='src/Logo_ASD.png'
+} else{
+   
+imgbotão.src='src/logoCanva.png'
+ 
+}
+
+botão2.textContent='';
+botão2.className=`fa-solid fa-download`;
+botão3.textContent='';
+botão3.className=`fa-solid fa-square-share-nodes`;
+
+
+botão1.appendChild(imgbotão);
+div1.appendChild(label1);
+div1.appendChild(br1); 
+div1.appendChild(img);
+div2.appendChild(label2); 
+div2.appendChild(br2); 
+div2.appendChild(label3); 
+div3.appendChild(botão1);
+div3.appendChild(botão2); 
+div3.appendChild(botão3); 
+div1.appendChild(div2);
+div1.appendChild(div3);
+list.appendChild(div1) ;
+ sessionStorage.setItem('pesQuiSar', '');
+ sessionStorage.setItem('itens',`${itens}`)
+
+ botão3.addEventListener('click',function(){
+var url = "https://carloseapp.github.io/Logo_ASD/AppWeb/Index.html";
+var img = `${doc.Titulo}: ${doc.URL}`;
+var cod=`${doc.Código}`
+var whatsappMessage =`Pagina Web: ${url}\n\n📷 ${img} \n\n Código: ${cod}\n\n`;
+var whatsappLink = `https://wa.me/?text=${encodeURIComponent(whatsappMessage)}`;
+
+window.open(whatsappLink, "_blank");
+ });
+
+botão1.addEventListener('click',function(){
+    if(doc.Canvas==''){
+       Swal.fire('desculpe-me!','Esse design não possui link para edição no Canva..')
+    } else{
+       window.open(`${doc.Canvas}`,'_blank')
+    }
+   
+});
+    
+})
+})
 
